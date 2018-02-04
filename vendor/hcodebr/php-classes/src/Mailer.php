b3c9b7d447225e7d63174da524023dc8281ef103
georgelucas <djgeorgelucas@hotmail.com>
2018-02-04 20:06:58 -0300

    Admin - Produtos

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -13,8 +13,8 @@ use Rain\Tpl;
 class Mailer
 {
 
-    const USERNAME = "djgeorgelucasgp@gmail.com";
-    const PASSWORD = "newlucas";
+    const USERNAME = "EMAIL";
+    const PASSWORD = "SENHA";
     const NAME_FROM = "LOJA VIRTUAL";
 
     private $mail;
