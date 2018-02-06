6ed678c9144f3aba7fca6e7bfc5d1a8fa7927e88
georgelucas <djgeorgelucas@hotmail.com>
2018-02-06 20:56:58 -0300

    Detalhes dos Produtos

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -115,10 +115,10 @@ class Product extends Model
     public function setPhoto($file)
     {
 
-        $extension = explode('.',$file['name']);
+        $extension = explode('.', $file['name']);
         $extension = end($extension);
 
-        switch ($extension){
+        switch ($extension) {
             case "jpg":
             case "jpge":
                 $image = imagecreatefromjpeg($file["tmp_name"]);
@@ -147,7 +147,30 @@ class Product extends Model
         $this->checkPhoto();
 
 
+    }
 
+    public function getFromURL($desurl)
+    {
+        $sql = new Sql();
+
+        $rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1",[
+            ':desurl'=>$desurl
+        ]);
+
+        $this->setData($rows[0]);
+
+    }
+
+    public function getCategories(){
+
+        $sql = new Sql();
+
+        return $sql->select("
+        SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct =
+        :idproduct
+        ",[
+            ':idproduct'=>$this->getidproduct()
+        ]);
     }
 
 }
