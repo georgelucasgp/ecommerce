7c74124b20f7afd367ac9942ca8739815bde5727
georgelucas <djgeorgelucas@hotmail.com>
2018-02-07 01:16:35 -0300

    ERRO CARRINHO

--- site.php
+++ site.php
@@ -3,6 +3,8 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
+use \Hcode\Model\User;
 
 $app->get('/', function () {
 
@@ -57,4 +59,14 @@ $app->get('/products/:desurl', function ($desurl){
         'product'=>$product->getValues(),
         'categories'=>$product->getCategories()
     ]);
+});
+
+$app->get('/cart', function (){
+
+    $cart = Cart::getFromSession();
+
+    $page = new Page();
+
+    $page->setTpl("cart");
+
 });
