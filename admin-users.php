ea1d0bd1059c32f5c85f1437f8a28e06ab1ec60e
georgelucasgp <djgeorgelucas@hotmail.com>
2018-02-23 01:15:34 -0300

    Admin - User - Search and Pagination

--- admin-users.php
+++ admin-users.php
@@ -13,12 +13,38 @@ $app->get('/admin/users', function () {
 
     User::verifyLogin();
 
-    $users = User::listAll();
+    $search = (isset($_GET['search'])) ? $_GET['search'] : "";
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+    if ($search != '') {
+
+        $pagination = User::getPageSearch($search, $page);
+
+    } else {
+
+        $pagination = User::getPage($page);
+    }
+
+
+    $pages = [];
+
+    for ($x = 0; $x < $pagination['pages']; $x++) {
+
+        array_push($pages, [
+            'href' => '/admin/users?' . http_build_query([
+                    'page' => $x + 1,
+                    'search' => $search
+                ]),
+            'text' => $x + 1
+        ]);
+    }
 
     $page = new PageAdmin();
 
     $page->setTpl("users", array(
-        "users" => $users
+        "users" => $pagination['data'],
+        "search" => $search,
+        "pages" => $pages
     ));
 
 
@@ -72,7 +98,7 @@ $app->post('/admin/users/create', function () {
 
     $user = new User();
 
-    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+    $_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
 
     $user->setData($_POST);
 
@@ -89,7 +115,7 @@ $app->post('/admin/users/:iduser', function ($iduser) {
 
     $user = new User();
 
-    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+    $_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
 
     $user->get((int)$iduser);
 
